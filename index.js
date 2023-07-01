f46a146346edba9f8e4982ba0d71a0382cf48471
Joseph Lurvey <joeylurvey@gmail.com>
2023-07-01 09:46:16 -0400

    changing name of repo

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"))
+document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"));
+
+//external API or json server with mock back-end
+//get working fetch request
+
+document.addEventListener("DOMContentLoaded", () => fetchData/*call with ()?*/);
+
+//hypothetically, get API to fetch data and log in console, then commit, comments present tense "working fetch"
+
+function fetchData() {
+    fetch('url')
+    .then((resp) => resp.json())
+    .then(() => console.log(data))
+};
